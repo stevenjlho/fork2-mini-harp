06a0f1b7891255ecd47023228c69e8807871f227
stevenjlho <stevenjlho@gmail.com>
2014-07-08 20:47:03 +0800

    Serving Static Content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-
-var createMiniHarp = require("../index.js")
-	  , app = createMiniHarp();
+var miniHarp = require("../index.js");
 
 var minimist = require('minimist');
 var argv = minimist(process.argv.slice(2));
 
+
 var port = argv.port || 4000;
-console.log(argv);
+
 console.log("Starting mini-harp on http://localhost:" + port);
+var root = argv._[0] || process.cwd();
+console.log(root);
+var app = miniHarp(root);
 	app.listen(port);
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-module.exports = function() {
-	var connect = require('connect');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+module.exports = function(path) {
 	// create a connect app
 	var app = connect();
-
+	app.use(function(request,response,next) {
+		if(request.url === '/current-time') {
+			// 如果没有newline,zsh默认会显示%
+			response.end((new Date()).toISOString() + '\n')
+		} else {
+			next();
+		}
+	}).use(serveStatic(path))
 	return app;
 }
